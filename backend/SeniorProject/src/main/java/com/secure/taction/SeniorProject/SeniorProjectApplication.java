ba6a5e3ff1751f120269329afa987523653fe255
B-Iglesia <bradiglesia@gmail.com>
2021-03-23 16:42:07 -0700

    ST-4: Started scripts for updating tables

--- backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/SeniorProjectApplication.java
+++ backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/SeniorProjectApplication.java
@@ -23,13 +23,13 @@ public class SeniorProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SeniorProjectApplication.class, args);
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-"))
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
             .build();
 
         DynamoDB dynamoDB = new DynamoDB(client);
 
         String tableName = "Movies";
-
+        /*
         try {
             System.out.println("Attempting to create table; please wait...");
             Table table = dynamoDB.createTable(tableName,
@@ -45,6 +45,20 @@ public class SeniorProjectApplication {
         }
         catch (Exception e) {
             System.err.println("Unable to create table: ");
+            System.err.println(e.getMessage());
+        }
+        */
+        Table table = dynamoDB.getTable("Movies");
+
+        try {
+            System.out.println("Attempting to delete table; please wait...");
+            table.delete();
+            table.waitForDelete();
+            System.out.print("Success.");
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to delete table: ");
             System.err.println(e.getMessage());
         }
 	}
