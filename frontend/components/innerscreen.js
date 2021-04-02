a0c5de23b5cc34899700db16cec70caf09d5d30e
stonewallstan <stanleyarmstrong31@gmail.com>
2021-04-02 10:17:44 -0700

    added the card and styles for the card, still working on the different types of inners

--- frontend/components/innerscreen.js
+++ frontend/components/innerscreen.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {Card} from 'react-native-paper';
-import {Text} from 'react-native';
+import {Card, Text, Divider} from 'react-native-paper';
+import {View} from 'react-native';
+import CardRow from './cardrow';
 
 const InnerScreen = (props) => {
   return (
-    <Card styles={styles.card}>
-      <Card.Title
-        title={props.title}
-        style={styles.title}
-        titleStyle={styles.titleColor}
-      />
-    </Card>
+    <View style={styles.card}>
+      <Card style={styles.inner}>
+        <Card.Title
+          title={props.title}
+          style={styles.title}
+          titleStyle={styles.titleColor}
+        />
+        <Card.Content>
+          <CardRow
+            bank={
+              'https://econlife.com/wp-content/uploads/2018/10/image_JP_Morgan_Chase_logo_-_Google_Search-1.jpg'
+            }
+            title={'Chase Ending in ...4415'}
+            progress={0.5}
+            alert={20.5}
+            cancel={300}
+            balance={3000}
+          />
+          <Divider />
+          <Text style={styles.newCard}> Add New Card </Text>
+        </Card.Content>
+      </Card>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  card: {
+    marginTop: 50,
+    marginLeft: 25,
+    marginRight: 25,
+  },
+  inner: {
+    shadowColor: '#000000',
+    shadowOpacity: 20,
+    borderRadius: 12,
+  },
   title: {
     color: '#FFFFFF',
     backgroundColor: '#00A7E1',
     fontSize: 24,
+    borderRadius: 12,
   },
   titleColor: {
     color: '#FFFFFF',
+  },
+  newCard: {
+    marginTop: 10,
+    textAlign: 'center',
   },
 });
 
