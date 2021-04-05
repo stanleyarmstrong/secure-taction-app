0a695b1fc5a71477951aaf090507f71e7ef6551b
B-Iglesia <bradiglesia@gmail.com>
2021-04-04 23:59:45 -0700

    ST-5: Tests (as of now) are complete

---
+++ backend/SeniorProject/src/test/java/com/secure/taction/SeniorProject/controllers/user/BaseControllerTest.java
@@ -0,0 +1,48 @@
+package com.secure.taction.SeniorProject.controllers.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.secure.taction.SeniorProject.controllers.UserController;
+import com.secure.taction.SeniorProject.repositories.UserRepository;
+import com.secure.taction.SeniorProject.services.UserService;
+import com.secure.taction.SeniorProject.tablesetup.controllers.CreditCardTableController;
+import com.secure.taction.SeniorProject.tablesetup.controllers.UserTableController;
+import com.secure.taction.SeniorProject.tablesetup.services.CreditCardTableService;
+import com.secure.taction.SeniorProject.tablesetup.services.UserTableService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class BaseControllerTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected UserController userController;
+
+    @MockBean
+    protected UserService userService;
+
+    @MockBean
+    protected UserRepository userRepository;
+
+    @MockBean
+    protected UserTableController userTableController;
+
+    @MockBean
+    protected UserTableService userTableService;
+
+    @MockBean
+    protected CreditCardTableController creditCardTableController;
+
+    @MockBean
+    protected CreditCardTableService creditCardTableService;
+
+}
