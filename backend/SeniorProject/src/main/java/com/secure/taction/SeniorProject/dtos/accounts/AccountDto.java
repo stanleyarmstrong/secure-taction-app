d3f19463e19355dc27502e2f09209773d3cd784c
stonewallstan <stanleyarmstrong31@gmail.com>
2021-04-16 00:06:08 -0700

    Added Account Information

---
+++ backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/dtos/accounts/AccountDto.java
@@ -0,0 +1,14 @@
+package com.secure.taction.SeniorProject.dtos.accounts;
+
+import java.math.BigDecimal;
+
+import org.springframework.lang.NonNull;
+
+public class AccountDto {
+  private String accountID;
+  private String userId;
+  private String accountType;
+  private BigDecimal balance;
+  private String accountName;
+
+}
---
+++ backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/dtos/accounts/AccountDtoToAccountItem.java
@@ -0,0 +1,5 @@
+package com.secure.taction.SeniorProject.dtos.accounts;
+
+public class AccountDtoToAccountItem {
+  
+}
