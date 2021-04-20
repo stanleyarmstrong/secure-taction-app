72ffdd5dd188cf879f1b994666d920131370c675
stonewallstan <stanleyarmstrong31@gmail.com>
2021-04-19 20:32:33 -0700

    AccountDTO converters have been set up

--- backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/dtos/accounts/AccountDtoToAccountItem.java
+++ backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/dtos/accounts/AccountDtoToAccountItem.java
@@ -1,5 +1,26 @@
 package com.secure.taction.SeniorProject.dtos.accounts;
 
-public class AccountDtoToAccountItem {
+import java.util.UUID;
+
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.secure.taction.SeniorProject.models.Account;
+import com.secure.taction.SeniorProject.tablesetup.constants.AccountTableConstants;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDtoToAccountItem implements Converter<AccountDto, Account>{
+  @Override
+  public Account convert(AccountDto dto) {
   
+    return new Account().withItem(
+      new Item()
+        .withPrimaryKey(AccountTableConstants.ACCOUNT_ID, UUID.randomUUID().toString().toUpperCase())
+        .with(AccountTableConstants.USER_ID, dto.getUserId())
+        .with(AccountTableConstants.ACCOUNT_TYPE, dto.getAccountType())
+        .with(AccountTableConstants.BALANCE, dto.getBalance())
+        .with(AccountTableConstants.ACCOUNT_NAME, dto.getAccountName())
+      );
+  } 
 }
--- backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/dtos/accounts/AccountItemtoAccountDto.java
+++ backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/dtos/accounts/AccountItemtoAccountDto.java
@@ -1,5 +1,26 @@
 package com.secure.taction.SeniorProject.dtos.accounts;
 
-public class AccountItemtoAccountDto {
-  
+import java.math.BigDecimal;
+import java.util.Map;
+
+import com.secure.taction.SeniorProject.models.Account;
+import com.secure.taction.SeniorProject.tablesetup.constants.AccountTableConstants;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@SuppressWarnings("unchecked")
+public class AccountItemtoAccountDto implements Converter<Account, AccountDto>{
+
+  @Override
+  public AccountDto convert(Account account) {
+    Map<String, Object> argItem = account.getAttributes();
+    return new AccountDto()
+      .withAccountId((String) argItem.get(AccountTableConstants.ACCOUNT_ID))     
+      .withUserId((String) argItem.get(AccountTableConstants.USER_ID))
+      .withAccountType((String) argItem.get(AccountTableConstants.ACCOUNT_TYPE))
+      .withBalance((BigDecimal) argItem.get(AccountTableConstants.BALANCE))
+      .withAccountName((String) argItem.get(AccountTableConstants.ACCOUNT_NAME));
+  }
 }
