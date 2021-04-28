91db855c0addf17794907f22ff9b92c3b68922d1
B-Iglesia <bradiglesia@gmail.com>
2021-04-28 15:19:19 -0700

    ST-43: Updated pom

---
+++ backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/constants/PlaidCredentials.java
@@ -0,0 +1,6 @@
+package com.secure.taction.SeniorProject.constants;
+
+public class PlaidCredentials {
+    public static String CLIENT_ID = "601ccb4f00e80f00113c58f5";
+    public static String SANDBOX_KEY = "e5ac68ca184449a580045975de6533";
+}
