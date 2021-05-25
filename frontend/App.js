4c714a508bdcf38467e2284bb0ee5a3f9b255132
stonewallstan <stanleyarmstrong31@gmail.com>
2021-05-24 17:30:28 -0700

    added menu for going to transaction alerts

--- frontend/App.js
+++ frontend/App.js
@@ -9,6 +9,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import Navbar from './components/navbar';
 import HomeScreen from './screens/Homescreen';
 import BudgetScreen from './screens/BudgetScreen';
@@ -17,42 +18,52 @@ import AddBudgetScreen from './screens/AddBudgetScreen';
 import Settings from './screens/Settings';
 
 const Stack = createStackNavigator();
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#00A7E1',
+    accent: '#ffffff',
+  },
+};
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="home"
-        screenOptions={{
-          header: (props) => <Navbar {...props} />,
-        }}>
-        <Stack.Screen
-          name="home"
-          component={HomeScreen}
-          options={{title: 'Good Morning, First Last'}}
-        />
-        <Stack.Screen
-          name="budget"
-          component={BudgetScreen}
-          options={{title: "Card's Budget"}}
-        />
-        <Stack.Screen
-          name="addbudget"
-          component={AddBudgetScreen}
-          options={{title: 'Add Budget'}}
-        />
-        <Stack.Screen
-          name="transactions"
-          component={TransactionsScreen}
-          options={{title: 'Recent Activity'}}
-        />
-        <Stack.Screen
-          name="settings"
-          component={Settings}
-          options={{title: 'Settings'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="home"
+          screenOptions={{
+            header: (props) => <Navbar {...props} />,
+          }}>
+          <Stack.Screen
+            name="home"
+            component={HomeScreen}
+            options={{title: 'Good Morning, First Last'}}
+          />
+          <Stack.Screen
+            name="budget"
+            component={BudgetScreen}
+            options={{title: "Card's Budget"}}
+          />
+          <Stack.Screen
+            name="addbudget"
+            component={AddBudgetScreen}
+            options={{title: 'Add Budget'}}
+          />
+          <Stack.Screen
+            name="transactions"
+            component={TransactionsScreen}
+            options={{title: 'Recent Activity'}}
+          />
+          <Stack.Screen
+            name="settings"
+            component={Settings}
+            options={{title: 'Settings'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
-import {Appbar} from 'react-native-paper';
+import {Appbar, Text, Menu} from 'react-native-paper';
 import {View} from 'react-native';
 
 const Navbar = ({navigation, previous, scene}) => {
+  const [visible, setVisible] = useState(false);
   const {options} = scene.descriptor;
   const leftIcon = previous ? 'chevron-left' : 'account-circle-outline';
   const rightIcon = previous ? 'home-outline' : 'bell';
@@ -15,37 +16,46 @@ const Navbar = ({navigation, previous, scene}) => {
           icon={leftIcon}
           size={35}
           onPress={() => {
-            if (leftIcon === 'chevron-left') {
-              navigation.pop();
-            } else {
-              navigation.push('settings');
-            }
+            return leftIcon === 'chevron-left'
+              ? navigation.pop()
+              : navigation.push('settings');
           }}
         />
         <Appbar.Content title={title} titleStyle={styles.title} />
-        <Appbar.Action
-          icon={rightIcon}
-          size={35}
-          onPress={() => {
-            if (rightIcon === 'bell') {
-              // neeed to change to show notifications in a different change
+        <Menu
+          visible={visible}
+          onDismiss={() => setVisible(false)}
+          anchor={
+            <Appbar.Action
+              icon={rightIcon}
+              size={35}
+              color="#FFFFFF"
+              onPress={() => {
+                return rightIcon === 'bell'
+                  ? setVisible(true)
+                  : navigation.popToTop();
+              }}
+            />
+          }>
+          <Menu.Item
+            title="Transaction Alert"
+            onPress={() => {
+              setVisible(false);
               navigation.push('settings');
-            } else {
-              navigation.popToTop();
-            }
-          }}
-        />
+            }}
+          />
+        </Menu>
       </Appbar.Header>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  barColor: {
-    backgroundColor: '#00A7E1',
-  },
   title: {
     fontSize: 18,
+  },
+  icon: {
+    backgroundColor: '#ffffff',
   },
 });
 
