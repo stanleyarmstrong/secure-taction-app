99ec21949f1fe9f1bf7902c9af86c356678fc9ee
B-Iglesia <bradiglesia@gmail.com>
2021-06-01 19:36:41 -0700

    fix

--- backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/controllers/PlaidController.java
+++ backend/SeniorProject/src/main/java/com/secure/taction/SeniorProject/controllers/PlaidController.java
@@ -150,7 +150,7 @@ public class PlaidController {
 
   }
   
-  @RequestMapping(value = "/transactions", method = RequestMethod.GET)
+  @RequestMapping(value = "/transactions", method = RequestMethod.POST)
   public ResponseEntity<Object> getTransactions(@RequestBody PlaidPublicTokenDto publicToken) throws Exception {
     if (publicToken.getPublicToken() == null) {
       return new ResponseEntity<>("Not Authorized Action", HttpStatus.UNAUTHORIZED);
